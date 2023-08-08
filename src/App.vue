03f72cd11855184ca9cccafb099506eec6348041
xiaofan10 <925158289@qq.com>
2023-08-08 16:49:31 +0800

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,5 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import { ref } from 'vue'
-const visible = ref<Boolean>(false)
-setTimeout(() => {
-  visible.value = true
-}, 3000)
 </script>
 
 <template>
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { inject, onMounted, ref } from 'vue'
-
+import { getDefaultConfig } from '@/utils/cesiumUtils'
 const cesiumContainer = ref<any>(null)
 
 const Cesium: any = inject('Cesium')
@@ -9,7 +9,7 @@ function initCesium() {
   // Cesium.Ion.defaultAccessToken =
   //   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJkY2UyZDI3NS02M2NjLTQ1OTUtODBmMC03YWNhYzk1NzU2M2MiLCJpZCI6MTU2MDQyLCJpYXQiOjE2OTAyMDc4ODR9.BmN3pOpnSPgJa2eBzYBHt5xrnMaIlV7MdcNrvowXpfs'
   viewer = new Cesium.Viewer(cesiumContainer.value, {
-    terrain: Cesium.Terrain.fromWorldTerrain()
+    ...getDefaultConfig()
   })
 }
 
