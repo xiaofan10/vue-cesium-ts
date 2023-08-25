f9ee6b936fe00c68b45cb3986771731f95d85a83
xiaofan10 <925158289@qq.com>
2023-08-25 13:47:40 +0800

    feat: Particle

--- src/views/CesiumEffects/Particle.vue
+++ src/views/CesiumEffects/Particle.vue
@@ -3,7 +3,27 @@
     <div class="cesium-container" ref="cesiumContainer"></div>
     <mars-dialog :visible="true" right="0" top="100" bottom="10" width="300" title="Particle">
       <p>
-        <a-space> heading-左右摇头 </a-space>
+        <a-space>
+          heading-左右摇头
+          <mars-input v-model:value="heading" @change="onHeadingPitchRollChange" type="number" />
+        </a-space>
+      </p>
+      <p>
+        <a-space>
+          pitch-抬头点头
+          <mars-input v-model:value="pitch" @change="onHeadingPitchRollChange" type="number" />
+        </a-space>
+      </p>
+      <p>
+        <a-space>
+          roll-翻转
+          <mars-input
+            v-model:value="roll"
+            @change="onHeadingPitchRollChange"
+            type="number"
+            step="0.1"
+          />
+        </a-space>
       </p>
     </mars-dialog>
     <div id="toolbar"></div>
@@ -14,61 +34,68 @@
 import { onMounted, ref } from 'vue'
 import * as Cesium from 'cesium'
 import { getDefaultConfig } from '@/utils/cesiumUtils'
+
+const heading = ref(0)
+const pitch = ref(0)
+const roll = ref(0)
+
 let viewer: Cesium.Viewer
 const cesiumContainer = ref<HTMLElement | null>(null)
-const time = Cesium.JulianDate.fromDate(new Date(2023, 8, 23, 16))
-const clock = {
-  start: time.clone(),
-  stop: Cesium.JulianDate.addSeconds(time, 120, new Cesium.JulianDate())
+let particleSystem: Cesium.ParticleSystem
+
+const computedModeMatrix = ({ lon, lat, height }) => {
+  const center = Cesium.Cartesian3.fromDegrees(lon, lat, height)
+  const matrix = Cesium.Transforms.eastNorthUpToFixedFrame(center)
+  // 将笛卡尔坐标位置转换成矩阵
+  return matrix
+}
+
+const computedEmitterModeMatrix = () => {
+  const hpr = new Cesium.HeadingPitchRoll(
+    Number(heading.value),
+    Number(pitch.value),
+    Number(roll.value)
+  )
+  const trs = new Cesium.TranslationRotationScale() // 用于平移 旋转 缩放 对象
+  trs.translation = new Cesium.Cartesian3(0, 0, 0)
+  trs.rotation = Cesium.Quaternion.fromHeadingPitchRoll(hpr)
+  const modelMatrix = new Cesium.Matrix4()
+  const matrix4 = Cesium.Matrix4.fromTranslationQuaternionRotationScale(
+    trs.translation,
+    trs.rotation,
+    trs.scale,
+    modelMatrix
+  )
+  return matrix4
+}
+
+const onHeadingPitchRollChange = (val: number) => {
+  if (particleSystem) {
+    particleSystem.emitterModelMatrix = computedEmitterModeMatrix()
+  }
 }
 
 const initClock = () => {
-  viewer.clock.startTime = clock.start.clone()
-  viewer.clock.stopTime = clock.stop.clone()
-  viewer.clock.currentTime = clock.start.clone()
-  viewer.clock.clockRange = Cesium.ClockRange.LOOP_STOP
-  viewer.clock.multiplier = 1
-  viewer.clock.shouldAnimate = true
-
-  const computedModeMatrix = ({ lon, lat, height }) => {
-    const center = Cesium.Cartesian3.fromDegrees(lon, lat, height)
-    const matrix = Cesium.Transforms.eastNorthUpToFixedFrame(center)
-    // 将笛卡尔坐标位置转换成矩阵
-    return matrix
-  }
-
-  const computedEmitterModeMatrix = () => {
-    const hpr = Cesium.HeadingPitchRoll.fromDegrees(0, 0, 10)
-    const trs = new Cesium.TranslationRotationScale()
-    trs.translation = Cesium.Cartesian3.fromElements(0, 0, 0)
-    trs.rotation = Cesium.Quaternion.fromHeadingPitchRoll(hpr)
-    const result = Cesium.Matrix4.fromTranslationRotationScale(trs)
-    console.log(result)
-    return result
-  }
-
+  viewer.clock.shouldAnimate = true // 是否应该动画
   // 创建粒子系统
-  var particleSystem = viewer.scene.primitives.add(
+  particleSystem = viewer.scene.primitives.add(
     new Cesium.ParticleSystem({
       image: '/assets/images/particle/smoke.png', // 设置粒子图片路径
+      imageSize: new Cesium.Cartesian2(1.0, 1.0),
       startColor: Cesium.Color.RED, // 起始颜色
       endColor: Cesium.Color.YELLOW, // 结束颜色
       startScale: 1.0, // 起始大小
       endScale: 10, // 结束大小
-      emissionRate: 100, // 每秒发射的粒子数量
+      emissionRate: 50, // 每秒发射的粒子数量
       minimumParticleLife: 2.0, // 最小粒子寿命
       maximumParticleLife: 5.0, // 最大粒子寿命
-      sizeInMeters: true, // 是否以米为单位计算粒子大小
+      sizeInMeters: false, // 是否以米为单位计算粒子大小
       minimumSpeed: 1.0, // 最小速度
-      maximumSpeed: 5.0, // 最大速度
+      maximumSpeed: 10.0, // 最大速度
       emitter: new Cesium.CircleEmitter(Cesium.Math.toRadians(20.0)), // 设置锥形发射器
-      lifetime: 10000.0, // 整体粒子系统的寿命
+      // lifetime: 10000.0, // 整体粒子系统的寿命
       modelMatrix: computedModeMatrix({ lon: -75.0, lat: 40.0, height: 1000.0 }), // 设置模型矩阵
-      emittterModelMatrix: computedEmitterModeMatrix() // 设置发射器模型矩阵
-      // emitterModelMatrix: Cesium.Matrix4.fromTranslation(
-      //   // 粒子发射器位置
-      //   Cesium.Cartesian3.fromDegrees(-75.0, 40.0, 100.0)
-      // )
+      emitterModelMatrix: computedEmitterModeMatrix() // 设置发射器模型矩阵
     })
   )
   // viewer.zoomTo(particleSystem)
@@ -78,33 +105,18 @@ const initClock = () => {
 
   // 设置摄像机位置和朝向，以便能够看到粒子系统
   camera.setView({
-    destination: Cesium.Cartesian3.fromDegrees(-75.0, 40.0, 1100.0), // 设置目标位置
+    destination: Cesium.Cartesian3.fromDegrees(-75.0, 39.9993, 1050.0), // 设置目标位置
     orientation: {
       heading: Cesium.Math.toRadians(0.0), // 设置朝向
       pitch: Cesium.Math.toRadians(-20.0), // 设置倾斜角度
       roll: 0.0 // 设置滚动角度
     }
   })
-  // camera.direction = Cesium.Cartesian3.normalize(
-  //   // 设置摄像机朝向
-  //   Cesium.Cartesian3.subtract(
-  //     Cesium.Cartesian3.fromDegrees(-75.0, 40.0, 1000.0), // 粒子系统的位置
-  //     camera.position,
-  //     new Cesium.Cartesian3()
-  //   ),
-  //   new Cesium.Cartesian3()
-  // )
-  // camera.up = new Cesium.Cartesian3(0.0, 0.0, 1.0)
 }
 
 const initCesium = () => {
   viewer = new Cesium.Viewer(cesiumContainer.value as HTMLElement, getDefaultConfig())
   viewer.scene.debugShowFramesPerSecond = true
-  // viewer.scene.debugShowCommands = false
-  // viewer.scene.debugShowFrustumPlanes = true
-  // viewer.scene.debugShowDepthFrustum = 1
-  // viewer.scene.globe.enableLighting = true
-  // viewer.scene.mode = Cesium.SceneMode.SCENE3D
   initClock()
 }
 
